12d85cb672873dbd8c439ff0cbe6fde319ac5e3c
Jakob Goes <jakob.goes@tum.de>
2019-12-07 00:50:38 +0100

    More tiles

---
+++ Assets/Scripts/DestructibleTile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "New Destructible Tile", menuName = "Tiles/Destructible Tile")]
+public class DestructibleTile : UnityEngine.Tilemaps.TileBase
+{
+
+}
