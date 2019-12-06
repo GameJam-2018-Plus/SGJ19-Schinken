82503ef9230dc9a5fdc864c892737b4213dbb611
Jakob Goes <jakob.goes@tum.de>
2019-12-06 22:49:55 +0100

    Background

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float damping;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, target.transform.position.x, damping), transform.position.y, transform.position.z);
+    }
+}
