a9da8cef5159a99111d07c95a9cb3b7b434c52b1
Sarah <sakobi7@googlemail.com>
2019-12-07 09:40:16 +0100

    formatierung

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private int lives=3;
+    private int lives = 3;
     private Rigidbody2D rb2d;
     private enum State
     {
@@ -13,61 +13,72 @@ public class Player : MonoBehaviour
         fridge
     }
     private State playerState;
-    [Range(0,20)]
-    public float speed=15;
-    [Range(0,10)]
-    public float jumpForce=7;
-    private float timeCounter=0;
-    [Range(0,100)]
-    public float fridgeGrav=70;
+    [Range(0, 20)]
+    public float speed = 15;
+    [Range(0, 10)]
+    public float jumpForce = 7;
+    private float timeCounter = 0;
+    [Range(0, 100)]
+    public float fridgeGrav = 70;
 
-    void move(){
-        float move=Input.GetAxis("Horizontal")*speed;
-        Vector2 speedVec=new Vector2(move, 0);
+    void move()
+    {
+        float move = Input.GetAxis("Horizontal") * speed;
+        Vector2 speedVec = new Vector2(move, 0);
         rb2d.AddForce(speedVec);
     }
 
-    void jump(){
-        float jump=0;
-        if(Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y-0.51f), Vector2.down, 0.01f)&&Input.GetButtonDown("Jump")){
-            jump=jumpForce;
+    void jump()
+    {
+        float jump = 0;
+        if (Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.51f), Vector2.down, 0.01f) && Input.GetButtonDown("Jump"))
+        {
+            jump = jumpForce;
         }
-        Vector2 jumpVec=new Vector2 (0, jump);
+        Vector2 jumpVec = new Vector2(0, jump);
         rb2d.AddForce(jumpVec, ForceMode2D.Impulse);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2d=GetComponent<Rigidbody2D>();
-        playerState=State.unarmed;
+        rb2d = GetComponent<Rigidbody2D>();
+        playerState = State.unarmed;
     }
 
     void FixedUpdate()
     {
-        if(Input.GetButtonDown("Fridge")){
-            if(playerState!=State.fridge){
-                playerState=State.fridge;
+        if (Input.GetButtonDown("Fridge"))
+        {
+            if (playerState != State.fridge)
+            {
+                playerState = State.fridge;
             }
-            else{
-                playerState=State.unarmed;
-            }
-        }
-        else if(Input.GetButtonDown("Schinken")){
-            if(playerState==State.armed){
-                playerState=State.unarmed;
-            }
-            else if(playerState!=State.fridge){
-                playerState=State.armed;
+            else
+            {
+                playerState = State.unarmed;
             }
         }
-        if(playerState!=State.fridge){
+        else if (Input.GetButtonDown("Schinken"))
+        {
+            if (playerState == State.armed)
+            {
+                playerState = State.unarmed;
+            }
+            else if (playerState != State.fridge)
+            {
+                playerState = State.armed;
+            }
+        }
+        if (playerState != State.fridge)
+        {
             move();
             jump();
         }
-        else{
-            rb2d.velocity=Vector2.zero;
-            rb2d.AddForce(Vector2.down*(-Physics2D.gravity)*fridgeGrav);
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(Vector2.down * (-Physics2D.gravity) * fridgeGrav);
         }
     }
 }
